42aa4141e9977fd0f468e8ae5f9d47b3070064ea
i3acsi <gasevskyv@gmail.com>
2020-12-12 18:26:15 +0700

    PostServlet - addPost

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -6,8 +6,10 @@ import ru.job4j.dream.model.Post;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Store {
+    private static AtomicInteger POST_ID = new AtomicInteger(4);
 
     private static final Store INST = new Store();
 
@@ -28,6 +30,10 @@ public class Store {
         return INST;
     }
 
+    public void save(Post post) {
+        post.setId(POST_ID.incrementAndGet());
+        posts.put(post.getId(), post);
+    }
 
     public Collection<Post> findAllPosts() {
         return posts.values();
